1664e726be75ab5c7977b2bba7781b785f86c8f3
saharshkrishna <68560607+saharshkrishna@users.noreply.github.com>
2025-10-25 21:30:44 +0530

    Fix: Update getLoanRe to getLoanReEntries

--- Backend/routes/loanRe.routes.js
+++ Backend/routes/loanRe.routes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { createLoanRe, getLoanRe } = require('../controllers/loanRe.controller');
-
+const { createLoanRe, getLoanReEntries } = require('../controllers/loanRe.controller');
 // POST route
 router.post('/', createLoanRe);
-
 // GET route
-router.get('/', getLoanRe);
-
+router.get('/', getLoanReEntries);
 module.exports = router;
