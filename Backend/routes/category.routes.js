ceb05a94094c8ca6ce25c477ae97536720cd1136
saharshkrishna <68560607+saharshkrishna@users.noreply.github.com>
2025-10-25 21:09:04 +0530

    Fix import path: change controller to controllers

--- Backend/routes/category.routes.js
+++ Backend/routes/category.routes.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { AddCategory, getCategory } = require("../controller/category.controller");
-
+const { AddCategory, getCategory } = require("../controllers/category.controller");
 router.post("/", AddCategory);
 router.get("/", getCategory);
-
 module.exports = router;
