63ef5a4c2ce36c9100024a63c1b5250c274b569d
saharshkrishna <68560607+saharshkrishna@users.noreply.github.com>
2025-10-25 21:04:02 +0530

    Create paymentMode.routes.js with AddPaymentMode and getPaymentModes routes

---
+++ Backend/routes/paymentMode.routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { AddPaymentMode, getPaymentModes } = require('../controllers/paymentMode.controller');
+
+// POST route for adding a payment mode
+router.post('/', AddPaymentMode);
+
+// GET route for getting payment modes
+router.get('/', getPaymentModes);
+
+module.exports = router;
