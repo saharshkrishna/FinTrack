abbe99c81aa9035f5a3db81457ac9adef2552c78
saharshkrishna <68560607+saharshkrishna@users.noreply.github.com>
2025-12-09 20:32:11 +0530

    Integrate sidebar collapse functionality in Layout
    - Added useSidebar hook to access collapse state
    - Made content margin dynamic based on sidebar state
    - Changes from ml-64 (when open) to ml-0 (when collapsed)
    - Added smooth transition animation for margin changes
    - Content now properly adjusts when sidebar toggles

--- Frontend/src/Layout.jsx
+++ Frontend/src/Layout.jsx
@@ -2,20 +2,26 @@ import PropTypes from 'prop-types';
 import Sidebar from './components/Sidebar'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useSidebar } from './context/SidebarContext';
 
 const Layout = ({children}) => {
+  const { isCollapsed } = useSidebar();
+
   return (
     <div className="min-h-screen bg-gray-50">
-        <Sidebar></Sidebar>
-        <div className='ml-64 p-6'>
+      <Sidebar></Sidebar>
+      <div className={`p-6 transition-all duration-300 ${
+        isCollapsed ? 'ml-0' : 'ml-64'
+      }`}>
         {children}
         <ToastContainer position="top-right" autoClose={3000} />
-          
-        </div>
+      </div>
     </div>
   )
 }
+
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 };
+
 export default Layout
